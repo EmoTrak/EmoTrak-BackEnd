8f0b9f194b7adc9aa0dd0667de994748de99e85d
JinguLeee <pingu514@naver.com>
2023-04-02 12:54:00 +0900

    feat : ci/cd 구축 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/SeoulCulturePort.jar"
+JAR_FILE="$PROJECT_ROOT/EmoTrak.jar"
 
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
