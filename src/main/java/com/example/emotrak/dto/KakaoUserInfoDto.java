8f05f42d988eb19e3dfdab04ea1241b162e96f67
JinguLeee <pingu514@naver.com>
2023-04-10 19:48:08 +0900

    fix : 카카오 로그인 닉네임 수정

--- src/main/java/com/example/emotrak/dto/KakaoUserInfoDto.java
+++ src/main/java/com/example/emotrak/dto/KakaoUserInfoDto.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 public class KakaoUserInfoDto {
     private Long id;
     private String email;
-
     private String nickname;
 
     public KakaoUserInfoDto(Long id, String email,String nickname) {
