8c1d604a549bbe41be045e648f484c64082f63ed
JinguLeee <pingu514@naver.com>
2023-04-13 02:27:51 +0900

    feat : 공유 중지 스웨거 추가

--- src/main/java/com/example/emotrak/controller/AdminController.java
+++ src/main/java/com/example/emotrak/controller/AdminController.java
@@ -3,6 +3,12 @@ package com.example.emotrak.controller;
 import com.example.emotrak.Service.AdminService;
 import com.example.emotrak.exception.ResponseMessage;
 import com.example.emotrak.security.UserDetailsImpl;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,9 +17,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Admin", description = "관리자")
 public class AdminController {
 
     private final AdminService adminService;
@@ -28,8 +36,25 @@ public class AdminController {
         return ResponseMessage.successResponse(HttpStatus.OK, "신고 댓글 조회 완료", adminService.reportComment(userDetails.getUser()));
     }
 
+    @Tag(name = "Admin")
+    @Operation(summary = "공유 중지", description = "신고가 많은 공유 글의 공유를 중지시킵니다.")
+    @ApiImplicitParams(
+            {
+                    @ApiImplicitParam(
+                            name = "boardId"
+                            , value = "공유 글 번호"
+                            , required = true
+                            , dataType = "Long"
+                            , paramType = "path"
+                            , defaultValue = "28"
+                    )
+            })
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "공유 중지 완료", response = ResponseMessage.class ),
+            @ApiResponse(code = 401, message = "권한이 없습니다", response = ResponseMessage.class )
+    })
     @PatchMapping("boards/restrict/{boardId}")
-    public ResponseEntity<?> restrictBoard(@PathVariable Long boardId, @AuthenticationPrincipal UserDetailsImpl userDetails){
+    public ResponseEntity<?> restrictBoard(@PathVariable Long boardId, @ApiIgnore @AuthenticationPrincipal UserDetailsImpl userDetails){
         adminService.restrictBoard(boardId, userDetails.getUser());
         return ResponseMessage.successResponse(HttpStatus.OK, "공유 중지 완료", null);
     }
