6b45f604a3709594166f4f2ad2bf4caff61aef26
JinguLeee <pingu514@naver.com>
2023-04-12 16:18:46 +0900

    fix : 공유게시판 글수정 에러메세지 수정

--- src/main/java/com/example/emotrak/Service/BoardService.java
+++ src/main/java/com/example/emotrak/Service/BoardService.java
@@ -56,7 +56,7 @@ public class BoardService {
     public void updateDaily(Long dailyId, BoardRequestDto boardRequestDto, User user, MultipartFile image) throws IOException {
         Daily daily = findDailyById(dailyId);
         if (daily.isHasRestrict() && daily.isShare()){
-            throw new CustomException(CustomErrorCode.UNAUTHORIZED_ACCESS);
+            throw new CustomException(CustomErrorCode.RESTRICT_ERROR);
         }
 
         validateUserOrAdmin(user, daily);
@@ -65,7 +65,6 @@ public class BoardService {
         Emotion emotion = findEmotionById(boardRequestDto.getEmoId());
         // Daily 객체 업데이트 및 저장
         daily.update(newImageUrl, boardRequestDto, emotion);
-        boardRepository.save(daily);
     }
 
     // 글 삭제
