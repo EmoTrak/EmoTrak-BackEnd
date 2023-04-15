43a4f3230aaf63a37946d67f8fe6afd4d15fb044
JinguLeee <pingu514@naver.com>
2023-04-15 17:47:47 +0900

    fix : 생성자 부분 수정

--- src/main/java/com/example/emotrak/dto/NaverUserInfoDto.java
+++ src/main/java/com/example/emotrak/dto/NaverUserInfoDto.java
@@ -2,8 +2,10 @@ package com.example.emotrak.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class NaverUserInfoDto {
     private String id; // 변경
