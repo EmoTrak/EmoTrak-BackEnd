3c79965fcad6dd97a405c7b22b718ac4de6841b4
bambee <daj0909@naver.com>
2023-05-03 00:03:59 +0900

    fix : 네이버 , 구글 주석 추가 , 구글 리펙토링

--- src/main/java/com/example/emotrak/service/NaverService.java
+++ src/main/java/com/example/emotrak/service/NaverService.java
@@ -53,7 +53,7 @@ public class NaverService {
         OauthUserInfoDto oauthUserInfo = getNaverUserInfo(accessToken);
         // 3. 필요시에 회원가입
         User naverUser = registerNaverUserIfNeeded(oauthUserInfo);
-        // 4. 사용자 엔티티에 리프레시 토큰 저장
+        // 4. 사용자 엔티티에 리프레시 토큰 저장 (연동해제를 위해 DB 저장)
         naverUser.updateNaverRefresh(refreshToken);
         // 5. JWT 토큰 반환
         TokenDto tokenDto = tokenProvider.generateTokenDto(naverUser, naverUser.getRole());
@@ -115,12 +115,13 @@ public class NaverService {
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode jsonNode = objectMapper.readTree(responseBody);
         JsonNode responseNode = jsonNode.get("response");
-        String id = responseNode.get("id").asText(); // 변경된 부분
+        String id = responseNode.get("id").asText(); // 변경된 부분(네이버의 경우 id 가 Long 값이다)
         String email = responseNode.get("email").asText();
         String nickname = responseNode.get("nickname").asText();
         return new OauthUserInfoDto(id, email, nickname);
     }
 
+    // 3. 필요시에 회원가입
     private User registerNaverUserIfNeeded (OauthUserInfoDto oauthUserInfo) {
         String naverId = oauthUserInfo.getId();
         User naverUser = userRepository.findByNaverId(naverId).
@@ -149,9 +150,9 @@ public class NaverService {
 
     // 네이버 연동해제
     public void unlinkNaver(User user) {
-        // DB에서 사용자의 리프레시 토큰 가져오기
+        // DB 에서 사용자의 리프레시 토큰 가져오기
         String refreshToken = user.getNaverRefresh();
-        // 리프레시 토큰을 사용하여 액세스 토큰 갱신
+        // 리프레시 토큰을 사용하여 액세스 토큰 갱신 (네이버의 경우 엑세스토큰이 유효하지 않더라도 success 를 반환)
         String accessToken = refreshAccessToken(refreshToken);
         if (accessToken == null) {
             throw new CustomException(CustomErrorCode.INVALID_OAUTH_TOKEN);
@@ -163,6 +164,7 @@ public class NaverService {
         }
     }
 
+    // 연동해제를 위한 네이버 API 호출
     private boolean unlinkNaverAccountApi(String accessToken) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
@@ -188,7 +190,7 @@ public class NaverService {
     // 리프레시 토큰을 사용하여 액세스 토큰 갱신
     private String refreshAccessToken(String refreshToken) {
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("grant_type", "refresh_token");
@@ -196,13 +198,13 @@ public class NaverService {
         body.add("client_secret", clientSecret);
         body.add("refresh_token", refreshToken);
 
-        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(body, headers);
-        RestTemplate rt = new RestTemplate();
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(body, headers);
+        RestTemplate restTemplate = new RestTemplate();
         try {
-            ResponseEntity<String> response = rt.exchange(
+            ResponseEntity<String> response = restTemplate.exchange(
                     "https://nid.naver.com/oauth2.0/token",
                     HttpMethod.POST,
-                    requestEntity,
+                    request,
                     String.class
             );
             if (response.getStatusCode() == HttpStatus.OK) {
