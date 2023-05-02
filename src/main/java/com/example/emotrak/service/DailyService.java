bf8addebba76ab81db9eb066b84b375f1b4838d9
JinguLeee <pingu514@naver.com>
2023-05-02 22:55:35 +0900

    fix : daily 수정

--- src/main/java/com/example/emotrak/service/DailyService.java
+++ src/main/java/com/example/emotrak/service/DailyService.java
@@ -19,14 +19,14 @@ import java.util.List;
 public class DailyService {
     private final DailyRepository dailyRepository;
 
-    @Transactional
+    @Transactional(readOnly = true)
     public DailyResponseDto getDailyMonth(int year, int month, User user) {
         List<DailyMonthResponseDto> dailyMonthResponseDtoList
                 = dailyRepository.getDailyMonth(year, month, user.getId());
         return new DailyResponseDto(year, month, dailyMonthResponseDtoList);
     }
 
-    @Transactional
+    @Transactional(readOnly = true)
     public DailyResponseDto getDailyDetail(Long dailyId, User user) {
         Daily daily = getDaily(dailyId);
         if (!daily.getUser().getId().equals(user.getId())) {
