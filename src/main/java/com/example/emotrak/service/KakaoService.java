06d509d1ca9a1c3c1efebe8a333523b1fad8dd16
bambee <daj0909@naver.com>
2023-05-07 01:29:49 +0900

    fix : 테스트코드 소셜작성중

--- src/main/java/com/example/emotrak/service/KakaoService.java
+++ src/main/java/com/example/emotrak/service/KakaoService.java
@@ -57,7 +57,7 @@ public class KakaoService {
     }
 
     // 1. "인가 코드"로 "액세스 토큰" 요청
-    private String getToken(String code) throws JsonProcessingException {
+    protected String getToken(String code) throws JsonProcessingException {
         // HTTP Header 생성
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
@@ -85,7 +85,7 @@ public class KakaoService {
     }
 
     // 2. 토큰으로 카카오 API 호출 : "액세스 토큰"으로 "카카오 사용자 정보" 가져오기
-    private OauthUserInfoDto getKakaoUserInfo(String accessToken) throws JsonProcessingException {
+    protected OauthUserInfoDto getKakaoUserInfo(String accessToken) throws JsonProcessingException {
         // HTTP Header 생성
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", "Bearer " + accessToken);
