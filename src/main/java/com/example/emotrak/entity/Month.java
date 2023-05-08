db7500f1cf333b5f554ff4e75046b06d8d9678aa
JinguLeee <pingu514@naver.com>
2023-05-08 18:44:20 +0900

    fix : 테스트 코드 수정

--- src/main/java/com/example/emotrak/entity/Month.java
+++ src/main/java/com/example/emotrak/entity/Month.java
@@ -1,13 +1,10 @@
 package com.example.emotrak.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import javax.persistence.*;
 
-@Setter
-@Getter
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "months")
