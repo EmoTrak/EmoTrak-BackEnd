120d2a152be23bc9febb8fa83ea18c7c523b81da
bambee <daj0909@naver.com>
2023-05-10 17:46:26 +0900

    fix : NaverServiceTest.java 테스트 3차 ( 중복된닉네임 , 중복된 이메일, 사용자가 이미 등록되어 있는경우 추가)

--- src/test/java/com/example/emotrak/service/NaverServiceTest.java
+++ src/test/java/com/example/emotrak/service/NaverServiceTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.ArgumentMatcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,6 +28,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -112,31 +115,359 @@ class NaverServiceTest {
         @Test
         @DisplayName("네이버유저가 존재하는 경우")
         void ExistingUser() throws JsonProcessingException {
+            // Access Token을 반환하는 Mock Response 설정
+            ResponseEntity<String> mockTokenResponse = new ResponseEntity<>(
+                    "{\"access_token\":\"mock_token\", \"refresh_token\":\"mock_refresh\"}",
+                    HttpStatus.OK
+            );
 
+            // User Info를 반환하는 Mock Response 설정
+            ResponseEntity<String> mockUserInfoResponse = new ResponseEntity<>(
+                    "{\"response\":{\"id\":\"1234\", \"nickname\":\"mock_nickname\", \"email\":\"mock@email.com\"}}",
+                    HttpStatus.OK
+            );
+
+            // "인가 코드"로 "액세스 토큰" 요청에 대한 Mocking
+            when(rt.exchange(
+                    eq("https://nid.naver.com/oauth2.0/token"),
+                    eq(HttpMethod.POST),
+                    any(HttpEntity.class),
+                    eq(String.class)
+            )).thenReturn(mockTokenResponse);
+
+            // 토큰으로 네이버 API 호출 : "액세스 토큰"으로 "네이버 사용자 정보" 가져오기에 대한 Mocking
+            when(rt.exchange(
+                    eq("https://openapi.naver.com/v1/nid/me"),
+                    eq(HttpMethod.POST),
+                    any(HttpEntity.class),
+                    eq(String.class)
+            )).thenReturn(mockUserInfoResponse);
+
+            // UserRepository Mocking
+            User existingUser = new User("encodedPassword", "existing_user_email", "existing_user_nickname", null, "1234", null, UserRoleEnum.USER);
+            when(userRepository.findByNaverId("1234")).thenReturn(Optional.of(existingUser));
+
+            // Mock TokenDto
+            TokenDto mockTokenDto = new TokenDto("Bearer","mock_access_token", "mock_refresh_token", 123L);
+            when(tokenProvider.generateTokenDto(existingUser, existingUser.getRole())).thenReturn(mockTokenDto);
+
+            // 테스트 수행
+            naverService.naverLogin("mock_auth_code", "mock_state", new MockHttpServletResponse());
+
+            // verify restTemplate.exchange 호출 검증
+            verify(rt, times(1)).exchange(
+                    eq("https://nid.naver.com/oauth2.0/token"),
+                    eq(HttpMethod.POST),
+                    any(HttpEntity.class),
+                    eq(String.class)
+            );
+
+            verify(rt, times(1)).exchange(
+                    eq("https://openapi.naver.com/v1/nid/me"),
+                    eq(HttpMethod.POST),
+                    any(HttpEntity.class),
+                    eq(String.class)
+            );
+
+            // verify userRepository.findByNaverId 호출 검증
+            verify(userRepository, times(1)).findByNaverId("1234");
+
+            // verify tokenProvider.generateTokenDto 호출 검증
+            verify(tokenProvider, times(1)).generateTokenDto(existingUser, existingUser.getRole());
         }
 
         @Test
         @DisplayName("중복된 이메일을 가진 사용자가 있는 경우")
         void ExistingUserWithSameEmail() throws JsonProcessingException {
+            // Access Token을 반환하는 Mock Response 설정
+            ResponseEntity<String> mockTokenResponse = new ResponseEntity<>(
+                    "{\"access_token\":\"mock_token\", \"refresh_token\":\"mock_refresh_token\"}",
+                    HttpStatus.OK
+            );
 
+            // User Info를 반환하는 Mock Response 설정
+            ResponseEntity<String> mockUserInfoResponse = new ResponseEntity<>(
+                    "{\"response\":{\"id\":\"mock_id\", \"email\":\"mock@email.com\", \"nickname\":\"mock_nickname\"}}",
+                    HttpStatus.OK
+            );
+
+            // Mock User
+            User mockUser = new User("encodedPassword", "mock@email.com", "mock_nickname", null, "mock_id", null, UserRoleEnum.USER);
+
+            // "인가 코드"로 "액세스 토큰" 요청에 대한 Mocking
+            when(rt.exchange(
+                    eq("https://nid.naver.com/oauth2.0/token"),
+                    eq(HttpMethod.POST),
+                    any(HttpEntity.class),
+                    eq(String.class)
+            )).thenReturn(mockTokenResponse);
+
+            // 토큰으로 네이버 API 호출 : "액세스 토큰"으로 "네이버 사용자 정보" 가져오기에 대한 Mocking
+            when(rt.exchange(
+                    eq("https://openapi.naver.com/v1/nid/me"),
+                    eq(HttpMethod.POST),
+                    any(HttpEntity.class),
+                    eq(String.class)
+            )).thenReturn(mockUserInfoResponse);
+
+            // UserRepository Mocking
+            when(userRepository.findByEmail("mock@email.com")).thenReturn(Optional.of(mockUser));
+            when(userRepository.save(any(User.class))).thenReturn(mockUser);
+
+            // Mock TokenDto
+            TokenDto mockTokenDto = new TokenDto("Bearer","mock_access_token", "mock_refresh_token", 123L);
+
+            // TokenProvider Mocking
+            when(tokenProvider.generateTokenDto(any(User.class), any(UserRoleEnum.class))).thenReturn(mockTokenDto);
+
+            // 테스트 수행
+            naverService.naverLogin("mock_auth_code", "mock_state", new MockHttpServletResponse());
+
+            // verify restTemplate.exchange 호출 검증
+            verify(rt, times(1)).exchange(
+                    eq("https://nid.naver.com/oauth2.0/token"),
+                    eq(HttpMethod.POST),
+                    any(HttpEntity.class),
+                    eq(String.class)
+            );
+
+            verify(rt, times(1)).exchange(
+                    eq("https://openapi.naver.com/v1/nid/me"),
+                    eq(HttpMethod.POST),
+                    any(HttpEntity.class),
+                    eq(String.class)
+            );
+
+            // verify userRepository.findByEmail 호출 검증
+            verify(userRepository, times(1)).findByEmail("mock@email.com");
+
+            // verify userRepository.findByNaverId 호출 검증
+            verify(userRepository, times(1)).findByNaverId("mock_id");
+
+            // verify userRepository.save 호출 검증
+            verify(userRepository, times(1)).save(any(User.class));
+
+            // verify tokenProvider.generateTokenDto 호출 검증
+            verify(tokenProvider, times(1)).generateTokenDto(any(User.class), any(UserRoleEnum.class));
         }
 
         @Test
         @DisplayName("중복된 이메일을 가진 사용자가 없는 경우")
         void NoExistingUserWithSameEmail() throws JsonProcessingException {
+            // Access Token을 반환하는 Mock Response 설정
+            ResponseEntity<String> mockTokenResponse = new ResponseEntity<>(
+                    "{\"access_token\":\"mock_token\", \"refresh_token\":\"mock_refresh_token\"}",
+                    HttpStatus.OK
+            );
 
+            // User Info를 반환하는 Mock Response 설정
+            ResponseEntity<String> mockUserInfoResponse = new ResponseEntity<>(
+                    "{\"response\":{\"id\":\"1234\", \"email\":\"mock@email.com\", \"nickname\":\"mock_nickname\"}}",
+                    HttpStatus.OK
+            );
+
+            // "인가 코드"로 "액세스 토큰" 요청에 대한 Mocking
+            when(rt.exchange(
+                    eq("https://nid.naver.com/oauth2.0/token"),
+                    eq(HttpMethod.POST),
+                    any(HttpEntity.class),
+                    eq(String.class)
+            )).thenReturn(mockTokenResponse);
+
+            // 토큰으로 네이버 API 호출 : "액세스 토큰"으로 "네이버 사용자 정보" 가져오기에 대한 Mocking
+            when(rt.exchange(
+                    eq("https://openapi.naver.com/v1/nid/me"),
+                    eq(HttpMethod.POST),
+                    any(HttpEntity.class),
+                    eq(String.class)
+            )).thenReturn(mockUserInfoResponse);
+
+            // UserRepository Mocking
+            when(userRepository.findByNaverId("1234")).thenReturn(Optional.empty());
+            when(userRepository.findByEmail("mock@email.com")).thenReturn(Optional.empty());
+
+            // TokenProvider Mocking
+            TokenDto mockTokenDto = new TokenDto("Bearer","mock_access_token", "mock_refresh_token", 123L);
+            when(tokenProvider.generateTokenDto(any(User.class), any(UserRoleEnum.class))).thenReturn(mockTokenDto);
+
+            // 테스트 수행
+            naverService.naverLogin("mock_auth_code", "mock_state", new MockHttpServletResponse());
+
+            // verify restTemplate.exchange 호출 검증
+            verify(rt, times(1)).exchange(
+                    eq("https://nid.naver.com/oauth2.0/token"),
+                    eq(HttpMethod.POST),
+                    any(HttpEntity.class),
+                    eq(String.class)
+            );
+
+            verify(rt, times(1)).exchange(
+                    eq("https://openapi.naver.com/v1/nid/me"),
+                    eq(HttpMethod.POST),
+                    any(HttpEntity.class),
+                    eq(String.class)
+            );
+
+            // verify userRepository.findByEmail 호출 검증
+            verify(userRepository, times(1)).findByEmail("mock@email.com");
+
+            // verify userRepository.findByNaverId 호출 검증
+            verify(userRepository, times(1)).findByNaverId("1234");
+
+            // verify userRepository.save 호출 검증
+            verify(userRepository, times(1)).save(any(User.class));
+
+            // verify tokenProvider.generateTokenDto 호출 검증
+            verify(tokenProvider, times(1)).generateTokenDto(any(User.class), any(UserRoleEnum.class));
         }
 
         @Test
         @DisplayName("중복된 닉네임이 존재할 경우")
-        void testHasNickname() throws JsonProcessingException  {
+        void testHasNickname() throws JsonProcessingException {
+            // Mock Responses
+            ResponseEntity<String> mockTokenResponse = new ResponseEntity<>(
+                    "{\"access_token\":\"mock_token\", \"refresh_token\":\"mock_refresh_token\"}",
+                    HttpStatus.OK
+            );
+            ResponseEntity<String> mockUserInfoResponse = new ResponseEntity<>(
+                    "{\"response\":{\"id\":\"1234\", \"email\":\"mock@email.com\", \"nickname\":\"mocknickname\"}}",
+                    HttpStatus.OK
+            );
 
+            // Mock User
+            User mockUser = new User(passwordEncoder.encode("randomPassword"), "mock@email.com", "mocknickname", null, "1234", null, UserRoleEnum.USER);
+
+            // Mocking
+            when(rt.exchange(
+                    eq("https://nid.naver.com/oauth2.0/token"),
+                    eq(HttpMethod.POST),
+                    any(HttpEntity.class),
+                    eq(String.class)
+            )).thenReturn(mockTokenResponse);
+
+            when(rt.exchange(
+                    eq("https://openapi.naver.com/v1/nid/me"),
+                    eq(HttpMethod.POST),
+                    any(HttpEntity.class),
+                    eq(String.class)
+            )).thenReturn(mockUserInfoResponse);
+
+            when(userRepository.findByNaverId("1234")).thenReturn(Optional.empty());
+            when(userRepository.findByEmail("mock@email.com")).thenReturn(Optional.empty());
+            when(userRepository.existsByNickname("mocknickname")).thenReturn(true);
+            when(userRepository.save(any(User.class))).thenReturn(mockUser);
+
+            TokenDto mockTokenDto = new TokenDto("Bearer", "mock_access_token", "mock_refresh_token", 123L);
+
+            when(tokenProvider.generateTokenDto(any(User.class), any(UserRoleEnum.class))).thenReturn(mockTokenDto);
+
+            // 테스트 수행
+            naverService.naverLogin("mock_code", "mock_state", new MockHttpServletResponse());
+
+            // verify restTemplate.exchange 호출 검증
+            verify(rt, times(1)).exchange(
+                    eq("https://nid.naver.com/oauth2.0/token"),
+                    eq(HttpMethod.POST),
+                    any(HttpEntity.class),
+                    eq(String.class)
+            );
+
+            verify(rt, times(1)).exchange(
+                    eq("https://openapi.naver.com/v1/nid/me"),
+                    eq(HttpMethod.POST),
+                    any(HttpEntity.class),
+                    eq(String.class)
+            );
+
+            // verify userRepository.getUniqueNameSuffix 호출 검증
+            verify(userRepository, times(1)).getUniqueNameSuffix("mocknickname");
+
+            // verify userRepository.save 호출 검증
+            verify(userRepository, times(1)).save(any(User.class));
+
+            // ArgumentCaptor를 이용해 userRepository.save()에 전달되는 User 객체 캡쳐
+            ArgumentCaptor<User> argument = ArgumentCaptor.forClass(User.class);
+            verify(userRepository).save(argument.capture());
+            // 캡쳐된 User 객체의 닉네임 접미사 검증
+            assertEquals("mocknickname_0", argument.getValue().getNickname());
         }
 
         @Test
         @DisplayName("중복된 닉네임이 존재하지 않을 경우")
         void testNoNickname() throws JsonProcessingException {
+            // Access Token을 반환하는 Mock Response 설정
+            ResponseEntity<String> mockTokenResponse = new ResponseEntity<>(
+                    "{\"access_token\":\"mock_token\", \"refresh_token\":\"mock_refresh_token\"}",
+                    HttpStatus.OK
+            );
 
+            // User Info를 반환하는 Mock Response 설정
+            ResponseEntity<String> mockUserInfoResponse = new ResponseEntity<>(
+                    "{\"response\":{\"id\":\"1234\", \"email\":\"mock@email.com\", \"nickname\":\"mocknickname\"}}",
+                    HttpStatus.OK
+            );
+
+            // Mock User
+            User mockUser = new User("encodedPassword", "mock@email.com", "mocknickname", null, "1234", null, UserRoleEnum.USER);
+
+            // "인가 코드"로 "액세스 토큰" 요청에 대한 Mocking
+            when(rt.exchange(
+                    eq("https://nid.naver.com/oauth2.0/token"),
+                    eq(HttpMethod.POST),
+                    any(HttpEntity.class),
+                    eq(String.class)
+            )).thenReturn(mockTokenResponse);
+
+            // 토큰으로 네이버 API 호출 : "액세스 토큰"으로 "네이버 사용자 정보" 가져오기에 대한 Mocking
+            when(rt.exchange(
+                    eq("https://openapi.naver.com/v1/nid/me"),
+                    eq(HttpMethod.POST),
+                    any(HttpEntity.class),
+                    eq(String.class)
+            )).thenReturn(mockUserInfoResponse);
+
+            // UserRepository Mocking
+            when(userRepository.findByNaverId("1234")).thenReturn(Optional.empty());
+            when(userRepository.findByEmail("mock@email.com")).thenReturn(Optional.empty());
+            when(userRepository.existsByNickname("mocknickname")).thenReturn(false);
+            when(userRepository.save(any(User.class))).thenReturn(mockUser);
+
+            // Mock TokenDto
+            TokenDto mockTokenDto = new TokenDto("Bearer","mock_access_token", "mock_refresh_token", 123L);
+
+            // TokenProvider Mocking
+            when(tokenProvider.generateTokenDto(any(User.class), any(UserRoleEnum.class))).thenReturn(mockTokenDto);
+
+            // 테스트 수행
+            naverService.naverLogin("mock_auth_code", "mock_state", new MockHttpServletResponse());
+
+            // verify restTemplate.exchange 호출 검증
+            verify(rt, times(1)).exchange(
+                    eq("https://nid.naver.com/oauth2.0/token"),
+                    eq(HttpMethod.POST),
+                    any(HttpEntity.class),
+                    eq(String.class)
+            );
+
+            verify(rt, times(1)).exchange(
+                    eq("https://openapi.naver.com/v1/nid/me"),
+                    eq(HttpMethod.POST),
+                    any(HttpEntity.class),
+                    eq(String.class)
+            );
+
+            // verify userRepository.getUniqueNameSuffix 호출 검증
+            verify(userRepository, times(0)).getUniqueNameSuffix("mocknickname");
+
+            // verify userRepository.save 호출 검증
+            verify(userRepository, times(1)).save(any(User.class));
+
+            // ArgumentCaptor를 이용해 userRepository.save()에 전달되는 User 객체 캡쳐
+            ArgumentCaptor<User> argument = ArgumentCaptor.forClass(User.class);
+            verify(userRepository).save(argument.capture());
+
+            // 캡쳐된 User 객체의 닉네임 검증
+            assertEquals("mocknickname", argument.getValue().getNickname());
         }
 
     }
@@ -259,7 +590,7 @@ class NaverServiceTest {
             User mockUser = new User();
             mockUser.setNaverRefresh("mock_refresh");
 
-            // Mock refresh token response
+            // 리프레시 토큰 요청을 Mocking
             ResponseEntity<String> mockRefreshTokenResponse = new ResponseEntity<>(
                     "{\"refresh_token\":\"mock_refresh\", \"access_token\":\"mock_token\"}",
                     HttpStatus.OK
@@ -271,7 +602,7 @@ class NaverServiceTest {
                     eq(String.class)
             )).thenReturn(mockRefreshTokenResponse);
 
-            // Mock failed unlink response
+            // 연동 해제 요청을 Mocking
             ResponseEntity<String> mockUnlinkResponse = new ResponseEntity<>(
                     "Unauthorized",
                     HttpStatus.UNAUTHORIZED
